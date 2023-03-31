8127011408269311f4b822034af4cfc94bcfa201
an4morph <avgustin456@gmail.com>
2023-03-31 20:40:27 +0600

    add login logic

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,7 +1,50 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { useState } from 'react'
+import { Form } from '../../components/form'
+import { TextField } from '../../components/text-field'
+import { authActions } from '../../store/auth/slices'
+
+
 export const LoginPage = () => {
+  const [form, setForm] = useState({
+    username: '',
+    password: '',
+  })
+
+  const dispatch = useDispatch()
+  const { status, error } = useSelector(state => state.auth.login)
+
+  const handleChange = (event) => {
+    const { name, value } = event.target
+    setForm({ ...form, [name]: value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    dispatch(authActions.login(form))
+  }
+
   return (
-    <div>
-      LoginPage
-    </div>
+    <Form 
+      title="Log in"
+      onSubmit={handleSubmit}
+      linkUnder={{ path: '/auth/signup', text: 'Go to sign up' }}
+    >
+      <div>{status}: {error}</div>
+      <TextField
+        type="text"
+        name="username"
+        placeholder="username"
+        value={form.username}
+        onChange={handleChange}
+      />
+      <TextField
+        type="password"
+        name="password"
+        placeholder="password"
+        value={form.password}
+        onChange={handleChange}
+      />
+    </Form>
   )
 }
