662b62b62385bcf1c9f19d46599c8c0e9f24d8d9
an4morph <avgustin456@gmail.com>
2023-04-07 23:35:56 +0600

    add adaptive for co-hero

--- src/constructor/co-hero/index.jsx
+++ src/constructor/co-hero/index.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components"
-import { Container } from '../../styles/adaptive'
+import { Container, media } from '../../styles/adaptive'
 
 const defaultValues = {
   bgColor: '#FFFFFF',
@@ -22,25 +22,50 @@ const Wrapper = styled.section`
   & > div {
     display: flex;
     align-items: center;
+    flex-direction: column;
   }
+
+  ${media.md`
+    & > div {
+      flex-direction: row;
+    }
+  `}
 `
 const TextContainer = styled.div`
-  padding-right: 80px;
+  text-align: center;
+  ${media.md`
+    text-align: left;
+    padding-right: 80px;
+  `}
 `
 const Title = styled.h1`
   color: ${({ $title }) => $title.color || defaultValues.title.color};
-  font-size: ${({ $title }) => $title.size || defaultValues.title.size}px;
+  font-size: ${({ $title }) => {
+    const size = ($title.size || defaultValues.title.size) * 0.8
+    return size < 20 ? 20 : size
+  }}px;
   line-height: 100%;
   margin-bottom: 30px;
+  ${media.md`
+    font-size: ${({ $title }) => $title.size || defaultValues.title.size}px;
+  `}
 `
 const Text = styled.p`
   color: ${({ $text }) => $text.color || defaultValues.text.color};
   font-size: ${({ $text }) => $text.size || defaultValues.text.size}px;
 `
 const Image = styled.img`
-  width: 40%;
-  height: 400px;
+  width: 100%;
+  height: 250px;
   object-fit: contain;
+  order: -1;
+  margin-bottom: 30px;
+  ${media.md`
+    order: 1;
+    width: 40%;
+    height: 400px;
+    margin-bottom: 0;
+  `}
 `
 
 export const CoHero = ({ 
