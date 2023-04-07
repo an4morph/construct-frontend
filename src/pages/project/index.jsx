af759c4485675a0db5d8b34e639d7116e560d051
an4morph <avgustin456@gmail.com>
2023-04-07 18:24:20 +0600

    add iframe 1 variant

--- src/pages/project/index.jsx
+++ src/pages/project/index.jsx
@@ -21,9 +21,6 @@ const Title = styled.h2`
   font-size: 20px;
   margin-bottom: 20px;
 `
-const Iframe = styled.iframe`
-  //height: 500px;
-`
 const IframeContainer = styled.div`
   border: 1px solid;
   width: 100%;
@@ -36,16 +33,14 @@ export const ProjectPage = () => {
   const iframeRef = useRef(null)
   
   const sizes = useSizes({
-    iframeW: iframeRef
+    iframe: iframeRef
   })
 
   useEffect(() => {
     fetch(`http://localhost:3789/projects/detail/${id}`)
       .then(res => res.json())
       .then(setData)
-  }, [id]) 
-
-  console.log(sizes)
+  }, [id])
 
   return data && (
     <Wrapper>
@@ -53,14 +48,14 @@ export const ProjectPage = () => {
         <Title>Project {data.name}: Result</Title>
 
       <IframeContainer ref={iframeRef}>
-        <Iframe
+        <iframe
           title="result"
           src={`/project/${id}/settings`}
           style={{ 
-            transform: `scale(${sizes.iframeW ? sizes.iframeW / window.innerWidth : 1})`,
+            transform: `scale(${sizes.iframe.width ? sizes.iframe.width / window.innerWidth : 1})`,
             transformOrigin: 'top left',
-            width: `${window.innerWidth / sizes.iframeW * 100}%`,
-            height: `${window.innerWidth / sizes.iframeW * 100}%`
+            width: `${window.innerWidth / sizes.iframe.width * 100}%`,
+            height: `${window.innerWidth / sizes.iframe.width * 100}%`
           }}
         />
       </IframeContainer>
