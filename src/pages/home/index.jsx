58dad263c4340d66a54af7ee7fe1b2e9b0829a34
myrzabekovv <akim.myrzabekov06@mail.ru>
2023-04-08 21:01:13 +0600

    AKI

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import styled from "styled-components"
 import { projectListActions } from "../../store/project-list/slices"
 import { ProjectListItem } from '../../components/project-list-item'
@@ -7,6 +7,7 @@ import { ProjectListItem } from '../../components/project-list-item'
 const Title = styled.h2`
   font-size: 20px;
   margin-bottom: 20px;
+  color: red;
 `
 const Grid = styled.div`
   font-size: 20px;
@@ -25,9 +26,12 @@ export const HomePage = () => {
   }, [dispatch, status])
   console.log(data)
 
+  const project = data || []
+  
+  
   return (
     <>
-      <Title>Dashboard</Title>
+      <Title>Dashboard {project.length} </Title>
       <Grid>
         {
           data && data.map((project) => (
