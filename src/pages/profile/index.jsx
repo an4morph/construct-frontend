5607d2bb16e96508738dbd1e4c0b900272445ce0
myrzabekovv <akim.myrzabekov06@mail.ru>
2023-04-11 20:53:57 +0600

    Pulled out the firstName

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,7 +1,15 @@
 import { DevelopmentPlug } from "../../components/plug"
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 export const ProfilePage = () => {
+  const user = useSelector((state) => state.auth.user);
+  const userName = user?.firstName || user?.username;
+
   return (
-    <DevelopmentPlug title="Profile Page" />
+     <div>
+      <h1>Hello, {userName}</h1>
+      <DevelopmentPlug title="Profile Page" />
+     </div>
   )
 }
