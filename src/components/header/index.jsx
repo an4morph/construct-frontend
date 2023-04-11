acb346da41d02c17804b726f20edb826fdd30dd7
bayellaisss <119422574+bayellaisss@users.noreply.github.com>
2023-04-11 19:33:09 +0600

    BIS-logo-link

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -45,7 +45,7 @@ export const Header = forwardRef((props, ref) => {
     <HeaderWrapper ref={ref}>
       <Content>
         <Left>
-        <LeftLink to="/"><Logo /></LeftLink>
+        <NavLink to="/"><Logo /></NavLink>
           <LeftLink to="/">Go to Dashboard</LeftLink>
           <LeftLink to="/about">About</LeftLink>
         </Left>
