99f94da9f82fc6181fa8b84afda47d784e5d7adf
EmirJavascript <orozbekovjob@gmail.com>
2023-04-13 23:47:11 +0600

    added link for student in footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,6 +1,8 @@
 import { forwardRef } from "react"
 import styled from "styled-components"
 import { Container } from "../../styles/adaptive"
+// import { Button } from "../button"
+// import { Link } from "react-router-dom"
 
 const FooterWrapper = styled.footer`
   background-color: ${({ theme }) => theme.colors.footer.bg};
@@ -12,12 +14,16 @@ const Content = styled(Container)`
   justify-content: center;
   padding: 40px;
 `
+const TagA = styled.a`
+  margin-left: 6px;
+  margin-right: 6px;
+`
 
 export const Footer = forwardRef((props, ref) => {
   return (
     <FooterWrapper ref={ref}>
       <Content>
-        Polina's students 2023 :&#x29; &copy;
+        Polina's <TagA href="https://codifylab.com/" target="_blank">students</TagA> 2023 :&#x29; &copy;
       </Content>
     </FooterWrapper>
   )
