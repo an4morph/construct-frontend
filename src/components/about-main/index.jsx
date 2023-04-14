fcf5a66dc6d325fb221ad0fa9543105f165faf54
EmirJavascript <orozbekovjob@gmail.com>
2023-04-15 02:40:41 +0600

    changed media quores and little details

--- src/components/about-main/index.jsx
+++ src/components/about-main/index.jsx
@@ -1,9 +1,26 @@
-import styled from "styled-components"
+import styled, { css } from "styled-components"
+
+const breakpoints = {
+  xs: 0,
+  sm: 576,
+  md: 768,
+  lg: 992,
+  xl: 1200,
+  xxl: 1400,
+}
+
+const media = Object.keys(breakpoints).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (max-width: ${breakpoints[label]}px) {
+      ${css(...args)}
+    }
+  `
+  return acc;
+}, {})
 
 const Container = styled.div`
   display: flex;
-  justify-content: center;
-  flex-direction: column;
+  align-items: center;
   background-color: ${({ theme }) => theme.colors.about.bg};
   color: ${({ theme }) => theme.colors.about.text};
   border: 2px dashed ${({ theme }) => theme.colors.about.border};
@@ -13,71 +30,47 @@ const Container = styled.div`
   padding: 30px;
   max-width: 100%;
   margin: 0 auto;
-`
-const ContainerAboutMain = styled.div`
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  width: 100%;
-  height: fit-content;
 
-  @media screen and (max-width: 991px) {
-    display: flex;
+  ${media.lg`
     flex-direction: column-reverse;
-  }
+  `}
 `
 const Title = styled.h2`
   font-size: 40px;
   line-height: 100%;
   margin-bottom: 30px;
-
-  @media screen and (max-width: 500px) {
-    font-size: 32px;
-  }
 `
 const Text = styled.p`
   font-size: 20px;
-
-  @media screen and (max-width: 500px) {
-    font-size: 18px;
-  }
 `
 const TextContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: flex-start;
   padding-right: 30px;
-
-  @media screen and (max-width: 991px) {
+  
+  ${media.lg`
     padding-right: 0;
-  }
+  `}
 `
+
 const Image = styled.img`
   width: 400px;
   height: 400px;
   object-fit: cover;
 
-  @media screen and (max-width: 991px) {
+  ${media.lg`
     width: 100%;
-    height: 100%;
-    margin-bottom: 30px;
-  }
+    height: auto;
+    margin-bottom: 15px;
+  `}
 `
 
-const DEFAULT_TITLE = 'Create your website in just a few minutes!'
-const DEFAULT_TEXT = 'Welcome to our website builder where you can create your own website in just a few minutes! Our intuitive interface allows even beginners in the world of web development to create beautiful and functional websites without any experience.'
-
-export const AboutMain = ({ className, title = DEFAULT_TITLE, text = DEFAULT_TEXT }) => {
+export const AboutMain = ({ className }) => {
   return (
-    <Container className={className}>      
-      <ContainerAboutMain >
-        <TextContainer>
-          <Title>{title}</Title>
-          <Text>{text}</Text>
-        </TextContainer>
-        <Image src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80" alt="#"/>
-      </ContainerAboutMain>
+    <Container className={className}>
+      <TextContainer>
+        <Title>Create your website in just a few minutes!</Title>
+        <Text>Welcome to our website builder where you can create your own website in just a few minutes! Our intuitive interface allows even beginners in the world of web development to create beautiful and functional websites without any experience.'</Text>
+      </TextContainer>
+      <Image src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80"/>
     </Container>
   )
 }
--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,4 +1,3 @@
-// import { DevelopmentPlug } from "../../components/plug"
 import { AboutMain } from "../../components/about-main";
 
 export const AboutPage = () => {
